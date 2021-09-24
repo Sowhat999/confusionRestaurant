25fa5c0753aa89f629998f7472e65de1fe4e27ff
dnaej <43270944+dnaej@users.noreply.github.com>
2021-09-24 09:03:59 -0400

    updated leader desc

--- src/shared/leaders.js
+++ src/shared/leaders.js
@@ -6,7 +6,7 @@ export const LEADERS = [
       designation: 'Chief Epicurious Officer',
       abbr: 'CEO',
       featured: false,
-      description: "Our CEO, Peter, credits his hardworking East Asian immigrant parents who undertook the arduous journey to the shores of America with the intention of giving their children the best future. His mother's wizardy in the kitchen whipping up the tastiest dishes with whatever is available inexpensively at the supermarket, was his first inspiration to create the fusion cuisines for which The Frying Pan became well known. He brings his zeal for fusion cuisines to this restaurant, pioneering cross-cultural culinary connections."
+      description: "Our CEO, Amanda, credits her hardworking East African immigrant parents who undertook the arduous journey to the shores of America with the intention of giving their children the best future. Her mother's wizardry in the kitchen whipping up the tastiest dishes with whatever is available inexpensively at the supermarket, was her first inspiration to create the fusion cuisines for which The Frying Pan became well known. She brings her zeal for fusion cuisines to this restaurant, pioneering cross-cultural culinary connections."
     },
     {
       id: 1,
@@ -15,7 +15,7 @@ export const LEADERS = [
       designation: 'Chief Food Officer',
       abbr: 'CFO',
         featured: false,
-      description: 'Our CFO, Danny, as he is affectionately referred to by his colleagues, comes from a long established family tradition in farming and produce. His experiences growing up on a farm in the Australian outback gave him great appreciation for varieties of food sources. As he puts it in his own words, Everything that runs, wins, and everything that stays, pays!'
+      description: 'Our CFO, Randa, as she is affectionately referred to by her colleagues, comes from a long established family tradition in farming and produce. Her experiences growing up on a farm in the Australian outback gave her great appreciation for varieties of food sources. As she puts it in her own words, Everything that runs, wins, and everything that stays, pays!'
     },
     {
       id: 2,
@@ -24,12 +24,12 @@ export const LEADERS = [
       designation: 'Chief Taste Officer',
       abbr: 'CTO',
         featured: false,
-      description: 'Blessed with the most discerning gustatory sense, Agumbe, our CFO, personally ensures that every dish that we serve meets his exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet his exacting standards. He lives by his motto, You click only if you survive my lick.'
+      description: 'Blessed with the most discerning gustatory sense, Sarah, our CFO, personally ensures that every dish that we serve meets her exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet her exacting standards. She lives by her motto, You click only if you survive my lick.'
     },
     {
       id: 3,
       name: 'Albert Somayya',
-      image: '/assets/images/Albert_Somayya.jpg',
+      image: '/assets/images/Feature_Albert.jpg',
       designation: 'Executive Chef',
       abbr: 'EC',
       featured: true,
